5ecaa062c5b50cc95a66b5218c0f0c63d3490adf
yangdefeng <defeng.yang@skilnets.com>
2020-07-27 11:18:22 +0800

    no message

---
+++ tests/test_reapet.py
@@ -0,0 +1,45 @@
+"""
+演示重复，由于封装错误的类造成的。模拟一个封装严重失误错误的封装例子。
+
+这个代码惨烈程度达到10级。明明是想记录10000次日志，结果却记录了 10000 * 10001 /2 次。如果把f函数调用100万次，那么控制台和文件将会各记录5000亿次，日志会把代码拖累死。
+不好好理解观察者模式有多惨烈。因为反复添加观察者（handler）,导致第1次调用记录1次，第二次调用时候记录2次，第10次调用时候记录10次，这成了高斯求和算法了。
+
+"""
+import logging
+import time
+
+
+class LogUtil:
+    def __init__(self):
+        self.logger = logging.getLogger('a')
+        self.logger.setLevel(logging.DEBUG)
+        self._add_stream_handler()
+        self._add_file_handler()
+
+    def _add_stream_handler(self):
+        sh = logging.StreamHandler()
+        sh.setFormatter(logging.Formatter(fmt="%(asctime)s-%(name)s-%(levelname)s-%(message)s"))
+        self.logger.addHandler(sh)
+
+    def _add_file_handler(self):
+        fh = logging.FileHandler('a.log')
+        fh.setFormatter(logging.Formatter(fmt="%(asctime)s-%(name)s-%(levelname)s-%(message)s"))
+        self.logger.addHandler(fh)
+
+    def debug(self, msg):
+        self.logger.debug(msg)
+
+    def info(self, msg):
+        self.logger.info(msg)
+
+
+def f(x):
+    log = LogUtil()  # 重点是这行，写在了函数内部。没有做命名空间的handlers判断控制，也没写单利或者享元模式。
+    log.debug(x)
+
+
+t1 = time.time()
+for i in range(10000):
+    f(i)
+
+print(time.time() - t1)
