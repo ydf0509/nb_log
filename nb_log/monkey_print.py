4070c3d59aba2242d68758effe12895061872bff
ydf0509 <ydf0509@qq.com>
2021-03-27 15:31:07 +0800

    1

--- nb_log/monkey_print.py
+++ nb_log/monkey_print.py
@@ -11,6 +11,13 @@ import traceback
 
 print_raw = print
 
+def stdout_write(msg:str):
+    sys.stdout.write(msg)
+    sys.stdout.flush()
+
+def stderr_write(msg:str):
+    sys.stderr.write(msg)
+    sys.stderr.flush()
 
 # noinspection PyProtectedMember,PyUnusedLocal,PyIncorrectDocstring
 def nb_print(*args, sep=' ', end='\n', file=None):
@@ -21,9 +28,7 @@ def nb_print(*args, sep=' ', end='\n', file=None):
     """
     args = (str(arg) for arg in args)  # REMIND 防止是数字不能被join
     if file == sys.stderr:
-        sys.stderr.write(sep.join(args))  # 如 threading 模块第926行，打印线程错误，希望保持原始的红色错误方式，不希望转成蓝色。
-        sys.stderr.flush()
-
+        stderr_write(sep.join(args)) # 如 threading 模块第926行，打印线程错误，希望保持原始的红色错误方式，不希望转成蓝色。
     elif file in [sys.stdout, None]:
         # 获取被调用函数在被调用时所处代码行数
         line = sys._getframe().f_back.f_lineno
@@ -31,13 +36,12 @@ def nb_print(*args, sep=' ', end='\n', file=None):
         file_name = sys._getframe(1).f_code.co_filename
         # sys.stdout.write(f'"{__file__}:{sys._getframe().f_lineno}"    {x}\n')
         if True:
-            sys.stdout.write(
+            stdout_write(
                 f'\033[0;34m{time.strftime("%H:%M:%S")}  "{file_name}:{line}"   \033[0;30;44m{sep.join(args)}\033[0m{end} \033[0m')  # 36  93 96 94
         else:
-            sys.stdout.write(
+            stdout_write(
                 f'\033[0;34m{time.strftime("%H:%M:%S")}  "{file_name}:{line}"   {sep.join(args)} {end} \033[0m')  # 36  93 96 94
         # sys.stdout.write(f'\033[0;30;44m"{file_name}:{line}"  {time.strftime("%H:%M:%S")}  {"".join(args)}\033[0m\n')
-        sys.stdout.flush()
     else:  # 例如traceback模块的print_exception函数 file的入参是   <_io.StringIO object at 0x00000264F2F065E8>，必须把内容重定向到这个对象里面，否则exception日志记录不了错误堆栈。
         print_raw(*args, sep=sep, end=end, file=file)
 
@@ -62,11 +66,9 @@ def print_exception(etype, value, tb, limit=None, file=None, chain=True):
             type(value), value, tb, limit=limit).format(chain=chain):
         # print(line, file=file, end="")
         if file != sys.stderr:
-            sys.stderr.write(f'{line} \n')
-            sys.stderr.flush()
+            stderr_write(f'{line} \n')
         else:
-            sys.stdout.write(f'{line} \n')
-            sys.stdout.flush()
+            stdout_write(f'{line} \n')
 
 
 
@@ -105,12 +107,9 @@ def common_print(*args, sep=' ', end='\n', file=None):
     args = (str(arg) for arg in args)
     args = (str(arg) for arg in args)  # REMIND 防止是数字不能被join
     if file == sys.stderr:
-        sys.stderr.write(sep.join(args) + end)  # 如 threading 模块第926行，打印线程错误，希望保持原始的红色错误方式，不希望转成蓝色。
-        sys.stderr.flush()
+        stderr_write(sep.join(args) + end) # 如 threading 模块第926行，打印线程错误，希望保持原始的红色错误方式，不希望转成蓝色。
     else:
-        sys.stdout.write(sep.join(args) + end)
-        sys.stdout.flush()
-
+        stdout_write(sep.join(args) + end)
 
 
 def reverse_patch_print():
