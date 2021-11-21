567ce8ae2560428b16edfea80bf9460037b8bb69
ydf0509 <123@gmail.com>
2021-11-21 22:51:49 +0800

    commit

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     platforms=["all"],
-    url='',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Operating System :: OS Independent',
