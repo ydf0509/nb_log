cd452da12eca5e7fd863ba8e94182e74d0921225
SC-202112251857\Administrator <ydf0509@sohu.com>
2022-01-08 21:52:55 +0800

    commit

--- tests/test6.py
+++ tests/test6.py
@@ -1,15 +1,19 @@
-from nb_log import get_logger,LogManager
-import requests
+# from nb_log import get_logger,LogManager
+# import requests
+#
+# get_logger('urllib3')
+# requests.get("http://www.baidu.com")
+#
+#
+# LogManager('abcd').preset_log_level(20)
+# l1 = get_logger('abcd',log_level_int=20)
+# l2 = get_logger('abcd',log_level_int=30)
+#
+#
+# l1.debug('能显示不？')
+#
+# l1.info('能显示不？')
 
-get_logger('urllib3')
-requests.get("http://www.baidu.com")
+import time
 
-
-LogManager('abcd').preset_log_level(20)
-l1 = get_logger('abcd',log_level_int=20)
-l2 = get_logger('abcd',log_level_int=30)
-
-
-l1.debug('能显示不？')
-
-l1.info('能显示不？')
+print(f'''\033[0;34m 前缀 \033[0;37;44mdfdsffdsf\033[0m ''')
