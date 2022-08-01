4353bebee74f4274f994a98e1afb4e65441e6b3e
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2022-08-01 14:38:57 +0800

    commit

--- tests/example.py
+++ tests/example.py
@@ -6,7 +6,7 @@ print('导入nb_log之前的print是普通的')
 
 from nb_log import get_logger
 
-logger = get_logger('lalala',log_filename='jinzhifengzhuang.log',formatter_template=5)
+logger = get_logger('lalala',log_filename='jinzhifengzhuang.log',formatter_template=5,log_file_handler_type=2)
 
 logger.debug(f'debug是绿色，说明是调试的，代码ok ')
 logger.info('info是天蓝色，日志正常 ')
--- tests/test_requests.py
+++ tests/test_requests.py
@@ -1,17 +1,26 @@
 import requests
 from nb_log import get_logger
+import pymysql
 
-logger = get_logger('urllib3', log_filename='urllib3.log', formatter_template=8,
-                    is_use_watched_file_handler_instead_of_custom_concurrent_rotating_file_handler=True)
+logger = get_logger('urllib3', log_filename='urllib3.log', )
+
+logger2 = get_logger('pymysql', log_filename='pymysql.log', )
 requests.get('http://www.baidu.com')
-logger.debug(11, extra={'c': 5, 'd': 6})
-logger.info(22)
-logger.warning(33)
-logger.error(44)
-logger.critical(55,extra=dict(f=7,g=8,h=9))
 
-logger.debug('哈哈哈哈', extra=dict(a=1, b=2))
-try:
-    1 / 0
-except Exception as e:
-    logger.exception('错误了')
+
+conn = pymysql.Connection(user='root',password='123456')
+cur = conn.cursor()
+
+cur.execute('select "abc" as x')
+print(cur.fetchall())
+# logger.debug(11, extra={'c': 5, 'd': 6})
+# logger.info(22)
+# logger.warning(33)
+# logger.error(44)
+# logger.critical(55,extra=dict(f=7,g=8,h=9))
+#
+# logger.debug('哈哈哈哈', extra=dict(a=1, b=2))
+# try:
+#     1 / 0
+# except Exception as e:
+#     logger.exception('错误了')
