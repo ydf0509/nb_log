956310994745b5f601bfd2461ec7d9929f10cd89
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2022-12-02 14:21:53 +0800

    commit

--- tests/test6.py
+++ tests/test6.py
@@ -19,22 +19,63 @@
 
 
 
-import  logging
-import nb_log
+# import  logging
+# import nb_log
 
-logger = logging.getLogger('a.b.c')
+# logger = logging.getLogger('a.b.c')
+#
+# logger.info('不会自动打印的')
+#
+# nb_log.get_logger(None)
+#
+# logger.info('这行会自动打印了，因为根命名空间加了handler')
+#
+#
+# logger1 = logging.getLogger('aaa')
+#
+# logger2 = logging.getLogger('aaa')
+#
+# logger3 = logging.getLogger('bbb')
+#
+# print('logger1 id: ',id(logger1),'logger2 id: ',id(logger2),'logger3 id: ',id(logger3))
 
-logger.info('不会自动打印的')
 
-nb_log.get_logger(None)
+#
+# import  logging
+# from  nb_log import get_logger
+#
+#
+# logger_foo = get_logger('foo',log_filename='foo.log')
+#
+# logger_bar = get_logger('bar',log_filename='bar.log')
+#
+#
+# logger_foo.debug('这句话将会写入foo.log文件')
+#
+# logger_bar.debug('这句话将会写入bar.log文件')
 
-logger.info('这行会自动打印了，因为根命名空间加了handler')
+import logging
+from nb_log.handlers import ConcurrentRotatingFileHandler,ColorHandler
+from nb_log.nb_log_config_default import FORMATTER_DICT
+
+logger = logging.getLogger('foo')
+
+logger.setLevel(logging.DEBUG)
+
+ch = ColorHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(FORMATTER_DICT[7])
+logger.addHandler(ch)
+
+fh = ConcurrentRotatingFileHandler('foo.log')
+fh.setLevel(logging.ERROR)
+fh.setFormatter(FORMATTER_DICT[11])
+logger.addHandler(fh)
 
 
-logger1 = logging.getLogger('aaa')
+logger.debug('debug debug')
+logger.info('info info')
+logger.warning('warning warning')
+logger.error('error error ')
+logger.critical('critical critical')
 
-logger2 = logging.getLogger('aaa')
-
-logger3 = logging.getLogger('bbb')
-
-print('logger1 id: ',id(logger1),'logger2 id: ',id(logger2),'logger3 id: ',id(logger3))
