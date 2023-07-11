e6a3dcef3aea2ef6ae8bc02883509b669b2b3f17
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2023-07-11 11:41:44 +0800

    commit

--- tests/test_use_curretn_dir_config/test_log_by_current_dir_config.py
+++ tests/test_use_curretn_dir_config/test_log_by_current_dir_config.py
@@ -8,7 +8,7 @@ print('导入nb_log之前的print是普通的')
 
 from nb_log import get_logger
 
-logger = get_logger('lalala',log_filename='jinzhifengzhuang.log',is_add_stream_handler=True,)
+logger = get_logger('lalala',log_filename='jinzhifengzhuang.log',is_add_stream_handler=False,)
 
 logger.debug(f'debug是绿色，说明是调试的，代码ok')
 logger.info('info是天蓝色，日志正常')
@@ -24,10 +24,10 @@ print('导入nb_log之后的print是强化版的可点击跳转的')
 import time
 t1 = time.time()
 for i in range(100000):
-    print(i)
+    # print(i)
     logger.debug(f'debug是绿色，说明是调试的，代码ok')
-    logger.info('info是天蓝色，日志正常')
-    logger.warning('黄色yello，有警告了')
-    logger.error('粉红色说明代码有错误')
-    logger.critical('血红色，说明发生了严重错误')
+    # logger.info('info是天蓝色，日志正常')
+    # logger.warning('黄色yello，有警告了')
+    # logger.error('粉红色说明代码有错误')
+    # logger.critical('血红色，说明发生了严重错误')
 print(time.time()-t1)
