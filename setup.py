dae796a86601680a9c14fd85c6f25ccbb72e254a
ydf0509 <909686719@qq.com>
2023-10-28 11:13:38 +0800

    commit

--- setup.py
+++ setup.py
@@ -28,9 +28,10 @@ install_requires = [
 if os.name == 'nt':
     install_requires.append('pywin32')
 
+print(f'nb_log install_requires:{install_requires}')
 setup(
     name='nb_log',  #
-    version="10.2",
+    version="10.5",
     description=(
         'very sharp color display,monkey patch bulitin print  and high-performance multiprocess safe roating file handler,other handlers includeing dintalk ,email,kafka,elastic and so on '
     ),
@@ -67,7 +68,7 @@ python setup.py sdist upload -r pypi
 
 
 python setup.py sdist & twine upload dist/nb_log-6.0.tar.gz
-python setup.py sdist & python -m  twine upload dist/nb_log-10.2.tar.gz
+python setup.py sdist & python -m  twine upload dist/nb_log-10.3.tar.gz
 
 twine upload dist/*
 
