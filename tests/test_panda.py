2bd137c065d32db2a5d9b5f4b0f7cdc869d81008
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2024-10-28 17:39:16 +0800

    commit

---
+++ tests/test_panda.py
@@ -0,0 +1,13 @@
+
+
+import pandas as pd
+
+# from nb_log import get_logger
+import nb_log
+
+data = [
+{"Name": "Alice", "Age": 25, "City": "New York"},
+{"Name": "Bob", "Age": 30, "City": "Los Angeles"}
+]
+
+df = pd.DataFrame(data)
